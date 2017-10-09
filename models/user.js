04359b7c6e9673a8283d1eea8e4c272b8f2300af
OmarNizam <eng.omarnizam@gmail.com>
2017-10-09 22:27:45 +0200

    Add authentication method, add session

--- models/user.js
+++ models/user.js
@@ -25,6 +25,29 @@ var UserSchema = new mongoose.Schema({
   }
 });
 
+// authenticate input against database documents
+UserSchema.statics.authenticate = function(email, password, callback) {
+  User.findOne({ email: email });  // tell mongoose to setup a query to find the document with the user's email address.
+      .exec(function (error, user) { // use the exec method to perform the search and provide a callback to process the result.
+        if(error) {
+          return callback(error);
+        } else if( !user ) {
+          var err =  new Error('User not found!!');
+          err.staus = 401;
+          return callback(err);
+        }
+        // use bcrypt.compare method to compare the supplied passwords with the hashed version.
+        // it returns error if something went wrong or result boolean : True if password match or False if not.
+        bcrypt.compare(password, user.password, function(error, result) {   // three arguments : login password, hashed password and a callback function.
+          if( result === true ) {
+            return callback(null, user);
+          } else {
+            return callback();
+          }
+        })
+      });
+}
+
 // hash password before saving to database by calling the pre method on our Schema
 // mongoose provides a function called pre-save hook
 UserSchema.pre('save', function(next) {   // save is the hook name
